b3c1f938d91e093b06eb8d2b6eb951c306098c1a
Bitt3rcrawlANA <722697@student.dusd.net>
2024-11-07 10:26:00 -0800

    awakeandstart
    calling start and awake

---
+++ ScriptingProjectP4Diaz/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("Awake callled.");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
